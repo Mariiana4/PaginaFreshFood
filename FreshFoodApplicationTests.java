6b8cf0cfad4a6d9adc7bd4fcaf9107da9994b62e
isa24dora <114768002+isa24dora@users.noreply.github.com>
2023-02-23 16:38:30 -0300

    Add files via upload

---
+++ FreshFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package Fresh.Food.fresquinhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
